5698fc8f7c1f7733cf27557c3bc47944c2d74ea5
JeongChangYong <ckddyd1202@daum.net>
2022-01-08 19:02:48 +0900

    Step 13
    SwiftUI Memo

--- SwiftUIMemo/SwiftUIMemo/Scene/DetailScene.swift
+++ SwiftUIMemo/SwiftUIMemo/Scene/DetailScene.swift
@@ -14,6 +14,9 @@ struct DetailScene: View {
     @EnvironmentObject var formatter: DateFormatter
     
     @State private var showEditSheet = false
+    @State private var showDeleteAlert = false
+    
+    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
         VStack {
@@ -35,17 +38,39 @@ struct DetailScene: View {
             
             HStack {
                 Button(action: {
+                    self.showDeleteAlert.toggle()
+                }, label: {
+                    Image(systemName: "trash")
+                        .foregroundColor(Color(UIColor.systemRed))
+                })
+                .padding()
+                .alert(isPresented: $showDeleteAlert, content: {
+                    Alert(title: Text("삭제 확인"), message: Text("메모를 삭제할까요?"),
+                          primaryButton:
+                            .destructive(Text("삭제"),
+                                action: {
+                                    self.store.delete(memo: self.memo)
+                                    self.presentationMode.wrappedValue.dismiss()
+                                }),
+                          secondaryButton: .cancel())
+                })
+                
+                Spacer()
+                
+                Button(action: {
                     self.showEditSheet.toggle()
                 }, label: {
                     Image(systemName: "square.and.pencil")
                 })
-                    .padding()
-                    .sheet(isPresented: $showEditSheet, content: {
-                        ComposeScene(showComposer: self.$showEditSheet, memo: self.memo)
-                            .environmentObject(self.store)
-                            .environmentObject(KeyboardObserver())
-                    })
+                .padding()
+                .sheet(isPresented: $showEditSheet, content: {
+                    ComposeScene(showComposer: self.$showEditSheet, memo: self.memo)
+                        .environmentObject(self.store)
+                        .environmentObject(KeyboardObserver())
+                })
             }
+            .padding(.leading)
+            .padding(.trailing)
         }
         .navigationBarTitle("메모 보기")
     }

cc86256bc80c31994fd825237dad95e6a789d3b0
JeongChangYong <ckddyd1202@daum.net>
2022-01-14 22:53:17 +0900

    Step 1
    SwiftUI Memo

---
+++ SwiftUIMemo/SwiftUIMemo/MemoListScene.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMemo
+//
+//  Created by 정창용 on 2022/01/14.
+//
+
+import SwiftUI
+import CoreData
+
+struct MemoListScene: View {
+    var body: some View {
+            Text("Hello, World!")
+    }
+}
+
+struct MemoListScene_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoListScene()
+    }
+}

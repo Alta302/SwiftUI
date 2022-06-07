4d278d6eb92266b71f85009f4312cd74c66cd36d
JeongChangYong <ckddyd1202@daum.net>
2022-06-07 19:50:04 +0900

    Weather App #1
    Weather App

---
+++ KxWeather/KxWeather/KxWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  KxWeatherApp.swift
+//  KxWeather
+//
+//  Created by 정창용 on 2022/06/07.
+//
+
+import SwiftUI
+
+@main
+struct KxWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ KxWeather/KxWeather/View/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  KxWeather
+//
+//  Created by 정창용 on 2022/06/07.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}

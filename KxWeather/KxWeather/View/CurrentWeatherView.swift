38c295c206c566eb929d74b1c4ad885ad308c2a4
JeongChangYong <ckddyd1202@daum.net>
2022-06-08 20:02:17 +0900

    Weather App #5
    Weather App

--- KxWeather/KxWeather/View/CurrentWeatherView.swift
+++ KxWeather/KxWeather/View/CurrentWeatherView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct CurrentWeatherView: View {
+    let model: CurrentWeather
+    
     var body: some View {
-        Color.yellow
-            .frame(height: 200)
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: model.icon)
+                Text(model.weather)
+            }
+            .font(.largeTitle)
+            
+            HStack(spacing: 20) {
+                Label(model.maxTemparature, systemImage: "arrow.up")
+                Label(model.minTemparature, systemImage: "arrow.down")
+            }
+            
+            HStack(alignment: .lastTextBaseline) {
+                Text(model.temparature)
+                    .font(.system(size: 120))
+                    .fontWeight(.ultraLight)
+                    .minimumScaleFactor(0.5)
+                
+                Spacer()
+                
+                HStack {
+                    Image(systemName: "sunrise.fill")
+                        .symbolRenderingMode(.multicolor)
+                    Text(model.sunrise)
+                    
+                    Image(systemName: "sunset.fill")
+                        .symbolRenderingMode(.multicolor)
+                    Text(model.sunset)
+                }
+                .font(.caption)
+            }
+        }
+        .foregroundColor(.white)
+        .padding(.horizontal)
     }
 }
 
 struct CurrentWeatherView_Previews: PreviewProvider {
     static var previews: some View {
-        CurrentWeatherView()
+        CurrentWeatherView(model: .preview)
     }
 }
